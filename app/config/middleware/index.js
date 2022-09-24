be289623f65a30316815d0de27cba91a667d25a4
ducph <duc.phamhuynh@ddsolution.vn>
2022-09-24 22:01:28 +0700

    Fix2

--- app/config/middleware/index.js
+++ app/config/middleware/index.js
@@ -1,51 +1,61 @@
-const jwt = require('jsonwebtoken');
-const { renderPage } = require('..');
-const { SECRET } = require('../../../constants');
-const User = require('../../models/user.model');
+const jwt = require('jsonwebtoken')
+const { renderPage } = require('..')
+const User = require('../../models/user.model')
 
 const unknownEndpoint = (request, response) => {
-  return renderPage(request, response)('', '', '/dang-nhap');
-};
+  return renderPage(request, response)('', '', '/dang-nhap')
+}
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-  const { path } = request;
+  console.error(error.message)
+  const { path } = request
   if (error.name === 'CastError') {
-    return renderPage(request, response)('', 'Có lỗi xảy ra. Vui lòng liên hệ admin!', '/dang-nhap');
+    return renderPage(request, response)(
+      '',
+      'Có lỗi xảy ra. Vui lòng liên hệ admin!',
+      '/dang-nhap'
+    )
   } else if (['JsonWebTokenError', 'TokenExpiredError'].includes(error.name)) {
-    return renderPage(request, response)('', 'Phiên đăng nhập hết hạn', '/dang-nhap');
-  }else if (error.name === 'SequelizeUniqueConstraintError') {
-    const { message, value } = error.errors[0];
-    return renderPage(request, response)('', `Vui lòng kiểm tra các trường dữ liệu: ${message} - ${value}`, path);
+    return renderPage(request, response)(
+      '',
+      'Phiên đăng nhập hết hạn',
+      '/dang-nhap'
+    )
+  } else if (error.name === 'SequelizeUniqueConstraintError') {
+    const { message, value } = error.errors[0]
+    return renderPage(request, response)(
+      '',
+      `Vui lòng kiểm tra các trường dữ liệu: ${message} - ${value}`,
+      path
+    )
   }
 
-  next(error);
-};
+  next(error)
+}
 
 const tokenExtractor = (req, res, next) => {
-  const authorization = req.cookies["Authorization"];
+  const authorization = req.cookies['Authorization']
   // token format: bearer xxx
   if (authorization && authorization.toLowerCase().startsWith('bearer')) {
-    req.token = authorization.substring(7);
+    req.token = authorization.substring(7)
   }
-  next();
-};
+  next()
+}
 
 const userExtractor = async (req, res, next) => {
-  const { token } = req;
-  const decodedToken = token && jwt.verify(token, SECRET);
+  const { token } = req
+  const decodedToken = token && jwt.verify(token, process.env.JWT_SECRET)
   if (!token || !decodedToken.id) {
-   
-    return renderPage(req, res)('', 'Vui lòng đăng nhập', '/dang-nhap');
+    return renderPage(req, res)('', 'Vui lòng đăng nhập', '/dang-nhap')
   }
-  const user = await User.findById(decodedToken.id);
-  req.user = user;
-  next();
-};
+  const user = await User.findById(decodedToken.id)
+  req.user = user
+  next()
+}
 
 module.exports = {
   unknownEndpoint,
   errorHandler,
   tokenExtractor,
   userExtractor,
-};
+}
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const path = require('path')
+const cors = require('cors')
 const flush = require('connect-flash')
-const { SECRET } = require('./constants')
-const { initDb } = require('./app/config')
 const middleware = require('./app/config/middleware')
+require('dotenv').config()
 
 const app = express()
 
@@ -23,7 +23,7 @@ const generateRouters = require('./app/routers')
 app.set('views', path.join(__dirname, 'build/views'))
 app.set('view engine', 'ejs')
 
-app.use(express.json())
+app.use(express.json(), cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
@@ -34,7 +34,7 @@ app.use('/assets', express.static(path.join(__dirname, 'build/assets')))
 
 app.use(
   session({
-    secret: SECRET,
+    secret: process.env.JWT_SECRET,
     cookie: {
       maxAge: 60000,
     },
@@ -64,7 +64,6 @@ app.use('/api', playListRouters)
 app.use('/api', songRouters)
 
 generateRouters(app)
-initDb()
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
